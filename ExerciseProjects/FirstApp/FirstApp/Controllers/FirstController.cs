b851d489306e614eb9048b0402c9ae879f9cc17c
xie_qiang <Xie_Qiang@turn2cloud.com>
2024-01-23 09:27:25 +0800

    First Application

---
+++ ExerciseProjects/FirstApp/FirstApp/Controllers/FirstController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstApp.Models;
+
+namespace FirstApp.Controllers
+{
+    public class FirstController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        public IActionResult Hello()
+        {
+            // ViewBag -> 動態對象
+            // ViewBag -> 可以被分配任何類型的變量
+            ViewBag.Message = "Hello World";
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            Person person = new Person();
+            person.Name = "John";
+            person.Age = 18;
+            person.Location = "United States";
+            
+            return View(person);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,3 +10,9 @@ git commit -m "first commit"
 git remote add origin https://github.com/lightblueskyline/YogiHostingExercise.git
 git push -u origin main
 ```
+
+## [ASP.NET Core Tutorials](https://www.yogihosting.com/category/aspnet-core/)
+
+### [Introduction to ASP.NET Core](https://www.yogihosting.com/aspnet-core-introduction/)
+
+### [First Application](https://www.yogihosting.com/aspnet-core-first-application/)
