2d8ffbd8000779f8413d5729590a7ea642a9ef2e
xie_qiang <Xie_Qiang@turn2cloud.com>
2024-04-25 11:57:31 +0800

    .NET Sqlite 創建數據庫

---
+++ ExerciseProjects/SqliteConsoleApp/SqliteConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace SqliteConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqliteConnectionStringBuilder scsb = new SqliteConnectionStringBuilder
+            {
+                DataSource = "ADO.sqlite",
+                Mode = SqliteOpenMode.ReadWriteCreate,
+                //Password = "MyEncryptionKey",
+            };
+
+            using (var connection = new SqliteConnection(scsb.ConnectionString))
+            {
+                connection.Open();
+                var createTableCommand = connection.CreateCommand();
+                createTableCommand.CommandText = @"
+CREATE TABLE IF NOT EXISTS Inventory
+(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL,
+    Price TEXT NOT NULL,
+    Quantity INTEGER NOT NULL,
+    AddedOn TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+";
+                createTableCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
